850b1e7944c25a8de2e3972207f9bc42c476ac3a
Ramesh prabhakar <rameshprabhakar014@gmail.com>
2025-12-22 10:30:10 +0530

    test route

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./useTheme/pages/Landing";
-import Login from "./useTheme/pages/Login";
-import Register from "./useTheme/pages/Register";
-import Dashboard from "./features/dashboard/Dashboard";
-import PaymentSelection from "./features/payment/PaymentSelection";
-import Logout from "./useTheme/pages/Logout";
-import ChooseLogin from "./useTheme/pages/ChooseLogin";
-import TeacherLogin from "./useTheme/pages/TeacherLogin";
-import ParentLogin from "./useTheme/pages/ParentLogin";
-import AdminLogin from "./useTheme/pages/AdminLogin";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-
-
-
+function Home() {
+  return <h1>Hello World</h1>;
+}
 
 function App() {
   return (
-  
-    <Router>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/payment" element={<PaymentSelection />} /> 
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/choose-login" element={<ChooseLogin />} />
-        <Route path="/teacher-login" element={<TeacherLogin />} />
-        <Route path="/parent-login" element={<ParentLogin />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-
-        
+        <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
